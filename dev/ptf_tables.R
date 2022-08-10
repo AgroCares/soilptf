23d5f9540f1a8457ab9824c3979b2e5ae26b4757
Gerard <gerard.ros@nmi-agro.nl>
2022-08-10 03:30:25 +0200

    extend table with ptf bulk density

--- dev/ptf_tables.R
+++ dev/ptf_tables.R
@@ -5,7 +5,7 @@
   require(usethis)
   
   # load the csv file with pdtf for bulk density
-  d1 <- fread('dev/sptf_bulkdensity.csv')
+  d1 <- fread('dev/sptf_bulkdensity.csv',na.strings=NULL)
 
   # make copy for package table
   sptf_bulkdensity <- copy(d1)
@@ -13,3 +13,26 @@
   # save updated crop table
   usethis::use_data(sptf_bulkdensity,overwrite = TRUE)
   
+  # make table for all countries (https://datahub.io/core/country-list#r)
+  
+    # get information where to find the location where csv data is stored
+    # library("jsonlite")
+    # json_file <- 'https://datahub.io/core/country-list/datapackage.json'
+    # json_data <- fromJSON(paste(readLines(json_file), collapse=""))
+    # json_data$resources$path
+    
+  # download data
+  d2 <- fread('https://pkgstore.datahub.io/core/country-list/data_csv/data/d7c9d7cfb42cb69f4422dec222dbbaa8/data_csv.csv', encoding = 'UTF-8')
+
+  # update column names
+  setnames(d2,c('country_name','country_code'))
+  
+  # set countries to lower case
+  d2[, country_name := tolower(country_name)]
+
+  # make copy for package table
+  sptf_countries <- copy(d2)
+  
+  # save updated crop table
+  usethis::use_data(sptf_countries,overwrite = TRUE)
+          
