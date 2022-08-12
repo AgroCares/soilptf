d7eaced440e9455e0c5951cf76e3685c1f46ec69
Gerard <gerard.ros@nmi-agro.nl>
2022-08-12 21:44:03 +0200

    fix utf-8 error in sptf_countries

--- dev/ptf_tables.R
+++ dev/ptf_tables.R
@@ -25,7 +25,7 @@
     # d2 <- fread('https://pkgstore.datahub.io/core/country-list/data_csv/data/d7c9d7cfb42cb69f4422dec222dbbaa8/data_csv.csv', encoding = 'UTF-8')
   
   # loaddata
-  d2 <- fread('dev/sptf_countries.csv')
+  d2 <- fread('dev/sptf_countries.csv',encoding = 'UTF-8')
   
   # update column names
   setnames(d2,c('country_name','country_code','continent_code'))
