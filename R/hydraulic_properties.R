10e1797a4e9ad5b32c9c643f6f053001c2dc13b8
Brent <brent.riechelman@xs4all.nl>
2023-03-15 17:18:31 +0100

    improve unit tests of hydraulic proptery ptfs

--- R/hydraulic_properties.R
+++ R/hydraulic_properties.R
@@ -20,10 +20,12 @@ sptf_vg1 <- function(A_CLAY_MI,A_SILT_MI,A_SOM_LOI,B_DEPTH) {
   
   # Check input
   arg.length <- max(length(A_CLAY_MI), length(A_SILT_MI), length(A_SOM_LOI), length(B_DEPTH))
-  checkmate::assert_numeric(A_CLAY_MI, lower = 0, upper = 100, any.missing = FALSE, min.len = 1)
-  checkmate::assert_numeric(A_SILT_MI, lower = 0, upper = 100, any.missing = FALSE, min.len = 1)
-  checkmate::assert_numeric(A_SOM_LOI, lower = 0, upper = 100, any.missing = FALSE, min.len = 1)
-  checkmate::assert_numeric(B_DEPTH, lower = 0, upper = 2, len = arg.length)
+  check_numeric('A_CLAY_MI', A_CLAY_MI, FALSE, arg.length)
+  check_numeric('A_SILT_MI', A_SILT_MI, FALSE, arg.length)
+  checkmate::assert_numeric(A_SOM_LOI, lower = get_minval('A_SOM_LOI'),
+                            upper = get_maxval('A_SOM_LOI'),
+                            any.missing = FALSE, min.len = 1)
+  check_numeric('A_DEPTH', A_DEPTH, FALSE, arg.length)
   
   # Collect data in a table
   dt <- data.table(id = 1:arg.length,
@@ -93,11 +95,15 @@ sptf_vg1 <- function(A_CLAY_MI, A_SILT_MI, A_SOM_LOI, A_SAND_M50, B_DEPTH){
   
   # Check input
   arg.length <- max(length(A_CLAY_MI), length(A_SILT_MI), length(A_SOM_LOI), length(A_SAND_M50), length(B_DEPTH))
-  checkmate::assert_numeric(A_CLAY_MI, lower = 0, upper = 100, any.missing = FALSE, min.len = 1)
-  checkmate::assert_numeric(A_SILT_MI, lower = 0, upper = 100, any.missing = FALSE, min.len = 1)
-  checkmate::assert_numeric(A_SOM_LOI, lower = 0, upper = 100, any.missing = FALSE, min.len = 1)
-  checkmate::assert_numeric(A_SAND_M50, lower = 0, upper = 2000, any.missing = FALSE, min.len = 1)
-  checkmate::assert_numeric(B_DEPTH, lower = 0, upper = 2, len = arg.length)
+  check_numeric('A_CLAY_MI', A_CLAY_MI, FALSE, arg.length)
+  check_numeric('A_SILT_MI', A_SILT_MI, FALSE, arg.length)
+  checkmate::assert_numeric(A_SOM_LOI, lower = get_minval('A_SOM_LOI'),
+                            upper = get_maxval('A_SOM_LOI'),
+                            any.missing = FALSE, min.len = 1)
+  checkmate::assert_numeric(A_SAND_M50, lower = get_minval('A_SAND_M50'),
+                            upper = get_maxval('A_SOM_LOI'),
+                            any.missing = FALSE, min.len = 1)
+  check_numeric('A_DEPTH', A_DEPTH, FALSE, arg.length)
   
   # Collect data in a table
   dt <- data.table(id = 1:arg.length,
