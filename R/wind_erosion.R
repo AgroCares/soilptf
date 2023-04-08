9525df8aa70658b78c085f5de7c652ba8abd5da1
Gerard <gerard.ros@nmi-agro.nl>
2023-04-08 17:35:04 +0200

    add sss2 to sss3

--- R/wind_erosion.R
+++ R/wind_erosion.R
@@ -282,7 +282,108 @@ sptf_sss1 <- function(A_SOM_LOI,A_CACO3_MI,A_DENSITY) {
   # select output variable
   value <- dt[,value]
   
-  # return value (mmol+ / kg)
+  # return value
+  return(value)
+  
+}
+
+# Predicting soil shear strength
+#
+#' Calculate the soil shear strength.
+#'
+#' This function calculates the soil shear strength via various soil properties
+#'
+#' @param A_SOM_LOI (numeric) The percentage organic matter in the soil (\%).
+#' @param A_CLAY_MI (numeric) The clay content of the soil (\%).
+#'
+#' @import data.table
+#' 
+#' @references Schjonning (2021) Topsoil shear strength – Measurements and predictions 
+#'
+#' @export
+sptf_sss2 <- function(A_SOM_LOI,A_CLAY_MI) {
+  
+  # Check input
+  arg.length <- max(length(A_SOM_LOI), length(A_CLAY_MI))
+  checkmate::assert_numeric(A_SOM_LOI, lower = 0, upper = 1000, len = arg.length)
+  checkmate::assert_numeric(A_CLAY_MI, lower = 0, upper = 100, len = arg.length)
+
+  # make internal data.table (clay and som in kg / kg)
+  dt <- data.table(id = 1: arg.length,
+                   A_C_OF = A_SOM_LOI * 10 * 0.5,
+                   A_SOM_LOI = A_SOM_LOI * 0.01,
+                   A_CLAY_MI = A_CLAY_MI * 0.01)
+  
+  # add normal load (in kPa, varying from 30-180 kPa in the experiments)
+  dt[, NL := 100]
+  
+  # estimate bulk density (in g / cm3)
+  dt[, bd := (1617 - 77.4 * log(A_C_OF) - 3.49 * A_C_OF) * 0.001]
+  
+  # estimate PPS, preload suction stress, calculated as the product of water saturation, S, and the numerical value of the matric potential
+  dt[, pps := 67]
+  
+  # Estimate soil shear strength across normal loads (R2 = 0.51, n = 931)
+  dt[, v1 := 46.5 + 0.60 * NL]
+  
+  # Estimate sss using bulk density (R2 = 0.91, n = 931), use PSS in hPa
+  dt[ ,v2 := -206 + 0.594 * NL + 78* A_CLAY_MI + 0.275 * pps + 123 * bd + 963 * A_SOM_LOI]
+  
+  # Estimate mean value
+  dt <- melt(dt,id.vars = 'id',measure.vars = c('v1','v2'))
+  dt <- dt[,list(value = mean(value,na.rm=T)),by='id']
+  
+  # select output variable
+  value <- dt[,value]
+  
+  # return value tau (kPa)
+  return(value)
+  
+}
+
+# Predicting soil shear strength
+#
+#' Calculate the soil shear strength.
+#'
+#' This function calculates the soil shear strength via various soil properties
+#'
+#' @param A_SOM_LOI (numeric) The percentage organic matter in the soil (\%).
+#' @param A_CLAY_MI (numeric) The clay content of the soil (\%).
+#'
+#' @import data.table
+#' 
+#' @references Schjonning (2021) Topsoil shear strength – Measurements and predictions. Using data from Schonning et al. (2020).
+#'
+#' @export
+sptf_sss2 <- function(A_SOM_LOI,A_CLAY_MI) {
+  
+  # Check input
+  arg.length <- max(length(A_SOM_LOI), length(A_CLAY_MI))
+  checkmate::assert_numeric(A_SOM_LOI, lower = 0, upper = 1000, len = arg.length)
+  checkmate::assert_numeric(A_CLAY_MI, lower = 0, upper = 100, len = arg.length)
+  
+  # make internal data.table (clay and som in kg / kg)
+  dt <- data.table(id = 1: arg.length,
+                   A_C_OF = A_SOM_LOI * 10 * 0.5,
+                   A_SOM_LOI = A_SOM_LOI * 0.01,
+                   A_CLAY_MI = A_CLAY_MI * 0.01)
+  
+  # add normal load (in kPa, varying from 30-180 kPa in the experiments)
+  dt[, NL := 100]
+  
+  # estimate bulk density (in g / cm3)
+  dt[, bd := (1617 - 77.4 * log(A_C_OF) - 3.49 * A_C_OF) * 0.001]
+  
+  # estimate PPS, preload suction stress, calculated as the product of water saturation, S, and the numerical value of the matric potential
+  dt[, pps := 67]
+  
+  # Estimate sss using bulk density (R2 = 0.84, n = 1651), use PSS in hPa
+  dt[ ,v2 := -188 + 0.558 * NL + 227* A_CLAY_MI + 0.220 * pps + 114 * bd + 1315 * A_SOM_LOI]
+  
+  # select output variable
+  value <- dt[,value]
+  
+  # return value tau (kPa)
   return(value)
   
 }
