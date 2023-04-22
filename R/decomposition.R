4bbe1c7315297ce28e580fc8c2111a6347ed7050
Gerard <gerard.ros@nmi-agro.nl>
2023-04-22 16:44:53 +0200

    add cdec2

--- R/decomposition.R
+++ R/decomposition.R
@@ -13,7 +13,7 @@
 #' @references Wosten et al. (1997) Bodemkundige vertaalfuncties bij SC-DLO; state of the art. https://edepot.wur.nl/299664
 #'
 #' @export
-sptf_cdec <- function(A_C_OF, A_N_RT, years) {
+sptf_cdec1 <- function(A_C_OF, A_N_RT, years) {
   
   # Check input
   arg.length <- max(length(A_N_RT),length(A_C_OF))
@@ -25,6 +25,38 @@ sptf_cdec <- function(A_C_OF, A_N_RT, years) {
   
   # estimate carbon decomposition
   value <- A_C_OF * (1 - exp(-kpot * years))
+  
+  # return value
+  return(value)
+  
+}
+
+#' Calculate the carbon decomposition for mineral soils using MINIP approach
+#'
+#' This function calculates the decomposition of sand and peat soils using first order kinetics.
+#'
+#' @param A_C_OF (numeric) The carbon content of the soil (g / kg).
+#' @param years (numeric) The years for which the carbon decomposition need to be estimated.
+#'
+#' @import data.table
+#' 
+#' @references Janssen (1984) A simple method for calculating decomposition and accumulation of 'young' soil organic matter.
+#'
+#' @export
+sptf_cdec2 <- function(A_C_OF, years) {
+  
+  # Check input
+  checkmate::assert_numeric(A_C_OF, lower = 0, upper = 1000)
+  checkmate::assert_int(years,lower = 1)
+  
+  # combine arguments in internal table
+  dt <- data.table(A_C_OF = A_C_OF,temp = 12)
+  
+  # add correction factor for annual temperature
+  dt[, cor_temp := ifelse(temp<=-1,0,ifelse(temp<=9,0.1*(temp+1),ifelse(temp<=27,2^((temp-9)/9),4)))]
+  
+  # estimate C decline via MINIP for other crops, 0-20 cm depth
+  dt[, value := A_C_OF * (1-exp(4.7*(((17+cor_temp*years)^-0.6)-(17^-0.6))))]
   
   # return value
   return(value)
